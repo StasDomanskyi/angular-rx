cd314dd8474d5fc5cdd4fdd918d297c4b099641d
StXt <stas.domanskiy@gmail.com>
2019-09-03 22:29:18 +0300

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ThingsComponent } from './pages/things/things.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'things', component: ThingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { FormComponent } from './form/form.component';
+import { FormComponent } from './components/form/form.component';
 import { FormsModule } from '@angular/forms';
 import { ImageService } from './services/image.service';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotificationsComponent } from './components/notifications/notifications.component';
+import { ThingsComponent } from './pages/things/things.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    MenuComponent,
+    HomeComponent,
+    NotificationsComponent,
+    ThingsComponent
   ],
   imports: [
     BrowserModule,
